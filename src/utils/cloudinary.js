534b07734e9cfc152528d915e4abfc5d19b88904
Nitish G. Saji <nitishsaji2001@gmail.com>
2024-09-07 12:20:31 +0530

    an update

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -16,8 +16,8 @@ const  uploadOnCloud = async (localFilePath) => {
             resource_type: "auto"
         })
         // file has been uploaded successfully
-        console.log("file is uploaded on cloudinary", response.url);
-
+        console.log("file is uploaded on cloudinary", response);
+        fs.unlinkSync(localFilePath)
         return response
     }catch (error){
         fs.unlinkSync(localFilePath) // remove the locally saved temp file as the upload operation got failed
